3ff14ce346e35e66e4a2b80d7e2340795ca63c30
Markusnl <markvandegiessen@hotmail.com>
2016-03-29 16:59:43 +0200

    added functionality to crypto

--- src/videostream/Crypto.java
+++ src/videostream/Crypto.java
@@ -1,31 +1,63 @@
 package videostream;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.net.UnknownHostException;
-import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
-import java.security.Security;
-import java.util.Arrays;
-
-//import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import java.security.SecureRandom;
+import org.bouncycastle.crypto.CipherParameters;
+import org.bouncycastle.crypto.Digest;
+import org.bouncycastle.crypto.StreamCipher;
+import org.bouncycastle.crypto.digests.SHA256Digest;
+import org.bouncycastle.crypto.engines.ChaChaEngine;
+import org.bouncycastle.crypto.macs.HMac;
+import org.bouncycastle.crypto.params.KeyParameter;
+import org.bouncycastle.crypto.params.ParametersWithIV;
+import org.bouncycastle.util.encoders.Hex;
 
 public class Crypto {
 
-    public static void main(String[] args) throws UnknownHostException, IOException, KeyManagementException, NoSuchAlgorithmException
-    {   
-       // Security.addProvider(new BouncyCastleProvider());
+    public byte[] addMac(byte[] key, byte[] data) throws NoSuchAlgorithmException, IOException {
+        Digest digest = new SHA256Digest();
+        HMac hmac = new HMac(digest);
+        hmac.init(new KeyParameter(key));
+        hmac.update(data, 0, data.length);
+        byte[] resBuf = new byte[digest.getDigestSize()];
+        hmac.doFinal(resBuf, 0);
+        String resStr = new String(Hex.encode(resBuf)); // Contains final usable value
+        
+        //append mac to data
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        outputStream.write(resBuf);
+        outputStream.write(data);
+        byte out[] = outputStream.toByteArray();
+        
+        return out;
+    }
 
-        //BC is the ID for the Bouncy Castle provider;
-        if (Security.getProvider("BC") == null){
-            System.out.println("Bouncy Castle provider is NOT available");
+    public byte[] ChaCha(byte[] key, byte[] nonce, byte[] in, boolean encrypt) {
+        //create chacha engine with key and IV
+        CipherParameters cp = new KeyParameter(key);
+        ParametersWithIV params = new ParametersWithIV(cp, nonce);
+        StreamCipher engine = new ChaChaEngine();
+        engine.init(encrypt, params);
+
+        //encrypt/decrypt and return output
+        byte out[] = new byte[in.length];
+        engine.processBytes(in, 0, in.length, out, 0);
+        return out;
+    }
+
+    public byte[] createRandom(int length) {
+        //create secure random key and nonce
+        byte random[] = new byte[length];
+        SecureRandom sr = null;
+        try {
+            sr = SecureRandom.getInstanceStrong();
+        } catch (NoSuchAlgorithmException ex) {
+            System.out.println("Security provider not available");
         }
-        else{
-            System.out.println("Bouncy Castle provider is available");
-        }
+        sr.nextBytes(random);
+        return random;
+    }
 
-        SSLSocketFactoryEx factory = new SSLSocketFactoryEx();
-        String[] cipherSuites = factory.GetCipherList();
-        System.out.println(Arrays.toString(cipherSuites));
-
-    } //end main
 }
--- src/videostream/Videostream.java
+++ src/videostream/Videostream.java
@@ -2,10 +2,12 @@ package videostream;
 
 import java.io.IOException;
 import static java.lang.Thread.sleep;
+import java.security.NoSuchAlgorithmException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.opencv.core.*;
 import org.opencv.videoio.*;
+import static javax.xml.bind.DatatypeConverter.printHexBinary;
 
 public class Videostream {
 
@@ -14,8 +16,42 @@ public class Videostream {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Videostream stream = new Videostream();
+        Crypto crypt = new Crypto();            
+        
+        byte key[] = crypt.createRandom(32);
+        byte nonce[] = crypt.createRandom(8);
+        
+        //encrypt test string with key and nonce
+        String test = "oke oke oke";
+        byte out[]=new byte[test.length()];
+        out=crypt.ChaCha(key,nonce,(byte[])test.getBytes(),true);
+        System.out.println("Ciphertext: "+printHexBinary(out));
+        
+        try {
+            out=crypt.addMac(key, out);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(Videostream.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Videostream.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        //decrypt test string with same key and nonce
+        //System.out.println(new String(crypt.ChaCha(key,nonce,out,true)));
+        //out=crypt.ChaCha(key,nonce,out,true);
+        
+        //split array
+        byte[] source = new byte[out.length];
+        byte[] part1 = new byte[32];
+        byte[] part2 = new byte[out.length-part1.length];
+
+        System.arraycopy(out, 0, part1, 0, part1.length);
+        System.arraycopy(out, part1.length, part2, 0, part2.length);
+        
+        System.out.println("SHA256:" +printHexBinary(part1));
+        System.out.println("Decoded PLAINTEXT:"+new String(crypt.ChaCha(key,nonce,part2,true)));
+
+       
         
         onvifControl onvifcamera = new onvifControl();
         try {
@@ -26,16 +62,27 @@ public class Videostream {
         } catch (IOException ex) {
             Logger.getLogger(Videostream.class.getName()).log(Level.SEVERE, null, ex);     
         }
-        
+         
         //stream.displayVideo("http://85.173.183.13/image1");
         //stream.displayVideo("http://d3macfshcnzosd.cloudfront.net/047802938_main_xxl.mp4");
         //stream.displayVideo("http://ak7.picdn.net/shutterstock/videos/2487797/preview/stock-footage-digital-countdown-timer-in-blue-color-over-black-background.mp4");
         //stream.displayVideo("http://administrator:thales@192.168.50.253/cgi-bin/nphcontinuousserverpush");
         /* TEST URLS
-            "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov"  
+        "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov"
+        "http://administrator:thales@192.168.50.253/cgi-bin/nphcontinuousserverpush"
+        http://ak7.picdn.net/shutterstock/videos/2487797/preview/stock-footage-digital-countdown-timer-in-blue-color-over-black-background.mp4
+         */ 
+            
+            //stream.displayVideo("http://85.173.183.13/image1");
+            //stream.displayVideo("http://d3macfshcnzosd.cloudfront.net/047802938_main_xxl.mp4");
+            //stream.displayVideo("http://ak7.picdn.net/shutterstock/videos/2487797/preview/stock-footage-digital-countdown-timer-in-blue-color-over-black-background.mp4");
+            //stream.displayVideo("http://administrator:thales@192.168.50.253/cgi-bin/nphcontinuousserverpush");
+            /* TEST URLS
+            "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov"
             "http://administrator:thales@192.168.50.253/cgi-bin/nphcontinuousserverpush"
-            http://ak7.picdn.net/shutterstock/videos/2487797/preview/stock-footage-digital-countdown-timer-in-blue-color-over-black-background.mp4  
-         */     
+            http://ak7.picdn.net/shutterstock/videos/2487797/preview/stock-footage-digital-countdown-timer-in-blue-color-over-black-background.mp4
+            */     
+       
     }
 
     public void displayVideo(String location) {
